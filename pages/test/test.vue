c3e60dcab7c73a2f71244d15e28741ca4602998f
Jedi-007 <1215072511@qq.com>
2023-05-31 12:54:42 +0800

    测试

---
+++ pages/test/test.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		444444444444
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
