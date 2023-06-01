6e04a86b76c42d8d6d3d5187dd352ab0fe308e3d
Jedi-007 <1215072511@qq.com>
2023-06-01 20:33:16 +0800

    xiugaibendi

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -36,7 +36,12 @@
 		},
 		methods : {
 			async getSwiperList(){
-				const {data: res}=	await uni.$http.get('/api/public/v1/home/swiperdata')
+				const {data: res}=	await
+				 
+				 
+				 
+				 
+			uni.$http.get('/api/public/v1/home/swiperdata')
 				//请求失败
 				if(res.meta.status !== 200) return uni.$showMsg()
 				this.swiperList = res.message
